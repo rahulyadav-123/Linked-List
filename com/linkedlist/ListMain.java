5c7dab1160986c25f198d06f39e5150f7080e0a3
rahul yadav <ry219613@gmail.com>
2021-10-25 23:36:01 +0530

    Delete last Node

--- com/linkedlist/ListMain.java
+++ com/linkedlist/ListMain.java
@@ -3,17 +3,18 @@ package com.linkedlist;
 public class ListMain {
 	public static void main(String[] args) {
 		
-	
-	LinkedList linkedList = new LinkedList();
-	linkedList.insert(56);
-	linkedList.insert(70);
-	System.out.println("Original list: ");
-	linkedList.display();
-	linkedList.insertInbtwn(30);
-	System.out.println("updated list: ");
-	linkedList.display();
-	linkedlist.pop();
-	System.out.println("After deleting the first node");
-	linkedList.display();
-	}
+		LinkedList linkedList = new LinkedList();
+		linkedList.insert(56);
+		linkedList.insert(70);
+		System.out.println("Original list: ");
+		linkedList.display();
+		
+		linkedList.insertInbtwn(30);
+		System.out.println("updated list: ");
+		linkedList.display();
+		
+		linkedList.popLast();
+		System.out.println("After deleting the Last node");
+		linkedList.display();
+		}
 }
